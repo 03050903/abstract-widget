7dcd394c623991dca6dc222848554e6b57c79006
tomwyr <tomwyr@hotmail.com>
2019-01-04 12:37:16 +0100

    Add docs

--- lib/abstract_widget.dart
+++ lib/abstract_widget.dart
@@ -6,8 +6,13 @@ typedef Widget ContextDerivedBuilder<T>(BuildContext context, T value);
 class AbstractWidget<T> extends StatelessWidget {
   AbstractWidget(this.value, {this.assertTypes = true});
 
+  /// Current value of the generic type [T].
   final T value;
+
+  /// Indicates whether widget should verify that reified type arguments of
+  /// registered builders are subtypes of [T].
   final bool assertTypes;
+
   final Map<Type, dynamic> _builders = Map();
   final Map<Type, dynamic> _contextBuilders = Map();
 
@@ -16,12 +21,16 @@ class AbstractWidget<T> extends StatelessWidget {
   List<Type> get _types =>
       []..addAll(_builders.keys)..addAll(_contextBuilders.keys);
 
+  /// Registers derived class builder that builds the widget depending
+  /// on current value of [T].
   void when<R extends T>(DerivedBuilder<R> builder) {
     _assertType(R);
     _contextBuilders.remove(R);
     _builders[R] = builder;
   }
 
+  /// Registers derived class builder that builds the widget depending
+  /// on [BuildContext] and current value of [T].
   void contextWhen<R extends T>(ContextDerivedBuilder<R> builder) {
     _assertType(R);
     _builders.remove(R);
@@ -30,7 +39,7 @@ class AbstractWidget<T> extends StatelessWidget {
 
   void _assertType(Type derivedType) {
     if (assertTypes) {
-      assert(derivedType != Null, _failedTypeMsg);
+      assert(derivedType != Null, _incompatibleTypeMsg);
     }
   }
 
@@ -44,8 +53,9 @@ class AbstractWidget<T> extends StatelessWidget {
       throw Exception("Attempted to build AbstractBuilder for unknown type: " +
           "${value.runtimeType}. Available types were: ${_types.toString()}.");
 
-  String get _failedTypeMsg =>
-      "Attempted to register DerivedBuilder of type that is not subtype of $T. " +
-      "Either make sure all AbstractWidget builders have correct type or " +
-      "disable type checking by setting \'assertTypes\' to false.";
+  String get _incompatibleTypeMsg =>
+      "Attempted to register DerivedBuilder of type argument that is not " +
+      "subtype of $T. Either make sure all AbstractWidget builders have " +
+      "correct type or disable type checking by setting \'assertTypes\' " +
+      "parameter to false.";
 }
